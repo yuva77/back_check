ddbf8735bffa6f1feff4d42f85f3bcd9af60dcef
yuva77 <53891265+yuva77@users.noreply.github.com>
2023-06-16 12:28:39 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const ip='10.7.120.195';
 app.use('/', router);
 app.use(errorLogger);
 
-const port = 8080;
+const port = 1050;
 
 app.listen(port,ip,()=>{
     console.log(`Server running at http://${ip}:${port}/`);
